3b1182d6e6f75b17fe572f312c3855606c18cc56
Max Wu <littlemengying@gmail.com>
2020-10-25 15:04:09 -0700

    add check file exist

--- products.py
+++ products.py
@@ -4,18 +4,24 @@ Created on Sat Oct 24 15:33:09 2020
 
 @author: littl
 """
-
 products = []
-with open("products.csv", "r") as f:
-    for line in f:
-        if"name, price" in line:
-            continue
-        name, price = line.strip().split(",")
-        products.append([name, price])
-print(products)
 
+import os
+#Check if the file is there
+if os.path.isfile("products.csv"):
+    print("Yeah! We found the file.")
+    # To read the file
+    with open("products.csv", "r") as f:
+        for line in f:
+            if"name, price" in line:
+                continue
+            name, price = line.strip().split(",")
+            products.append([name, price])
+    print(products)
+else:
+    print("Can't find the file....")
 
-
+# To input the product name
 while True: 
     name = input("Please enter product's name: ")
     if name == "q":   # quit 
@@ -24,12 +30,11 @@ while True:
     products.append([name, price])
 print(products)
 
-
-
+# To print the purchasing records
 for p in products: 
     print(p[0], "'s price is", p[1])
     
-
+# To write the file
 with open ("products.csv", "w") as f:
         f.write("name, price\n")
         for p in products:
